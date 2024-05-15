51cb124ace0e10aec35771de419dfed2ae666302
unknown <anoldanotidachitereki@gmail.com>
2024-05-15 11:53:07 +0200

    new updates

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let hamburger-menu = document.querySelector(`.hamburger-mennu`);
+let navlist = document.querySelector (`.navlist`);
+hamburger-menu.onclick = () =>{
+    hamburger-menu.classlist
+}
